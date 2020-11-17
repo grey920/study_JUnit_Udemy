3887674ec389a5144edcaa5e772230c8a4eb3067
gwjung <charminggw@gmail.com>
2020-11-17 18:29:31 +0900

    ✅ First Unit Test!

--- junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
+++ junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
@@ -4,13 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-// 항상 소스코드와 테스트 코드는 다른 폴더에 위치해야 한다
 public class MyMathTest {
-
-	@Test // unit테스트를 포함한다는 뜻
-	public void test() {
-		// 빈 테스트를 실행해도 초록막대가 뜬다 -> 실패가 없으면 성공이다
-		// 코드에서 예외가 발생하지 않았거나 모든 검사가 통과되면 테스트는 성공한다.
+	MyMath myMath = new MyMath();
+	
+	//MyMath.sum
+	// 1, 2, 3, => 6
+	@Test 
+	public void sum_with3numbers() {
+		// inline 만드는 단축키!! 원하는 변수 잡고 alt+ shift + i
+		assertEquals(6, myMath.sum(new int[] {1,2,3}));
+	}
+	
+	@Test 
+	public void sum_with1numbers() {
+		assertEquals(3, myMath.sum(new int[] {3}));
 	}
 
 }
